454cd9e8d9014769261a6275ab4703747456dfb9
kimeuihyun <qspp@daum.net>
2022-10-09 13:45:55 -0700

    binary search

--- Binary Search/11663.py
+++ Binary Search/11663.py
@@ -11,34 +11,42 @@ for i in range(m):
     l = tuple(map(int,sys.stdin.readline().split()))
     line.append(l)
 
-for i in range(n):
-
-    line_first = line[i][0]
-    line_last = line[i][1]
+    lower = l[0]
+    upper = l[1]
 
     start = 0
-    end = len(line)-1
-
+    end = len(dot)-1
+    lower_start = 0
     while start <= end:
-        middle = (start + end) // 2
-        if line_first < dot[middle]:
-            end = middle-1
-        else:
-            start = middle+1
-    x = start
+        middle = (start+end) // 2
 
-    start = 0
-    end = len(line)-1
-    while start <= end:
-        middle = (start + end) // 2
-        if line_last < dot[middle]:
+        if lower < dot[middle]:
+            lower_start = middle
             end = middle - 1
+        elif lower == dot[middle]:
+            lower_start = middle
+            break
+        else:
+            lower_start = middle+1
+            start = middle + 1
+    start = 0
+    end = len(dot) - 1
+    upper_end = 0
+    while start <= end:
+        middle = (start+end) // 2
+        if upper < dot[middle]:
+            end = middle - 1
+            upper_end = end
+        elif upper == dot[middle]:
+            upper_end = middle
+            break
         else:
             start = middle + 1
-    y = start
-    result = y - x + 1
-    print(y)
-    print(x)
-    #print(result)
+            upper_end = middle
+
+    if lower_start > upper_end:
+        print(0)
+    else:
+        print(upper_end - lower_start + 1)
 
 
