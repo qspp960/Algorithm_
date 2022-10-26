a27928776c222de3125bc07880bc4cf764d05e12
kimeuihyun <qspp@daum.net>
2022-10-26 08:53:36 -0700

    dynamic programming

---
+++ Dynamic Programming/11053.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+number = list(map(int,input().split()))
+
+dp = [0] * n
+
+for i in range(n):
+    for j in range(i):
+        if number[i] > number[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(max(dp))
